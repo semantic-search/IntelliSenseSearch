dd2faacadca8663942c00f73c912c1aca28ac02d
Adarsh Gupta <adarshguptamaurya@gmail.com>
2020-10-30 14:53:11 +0530

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -7,7 +7,7 @@ from elasticsearch import Elasticsearch
 
 
 def index():
-    client = Elasticsearch('localhost:9200')
+    client = Elasticsearch('13.68.241.106:9200')
     client.indices.delete(index="semantic", ignore=[404])
     with open("index.json") as index_file:
         source = index_file.read().strip()
