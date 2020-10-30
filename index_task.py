e44688d7ff47bb55b093e0dbfedf33ead1077d60
Adarsh Gupta <adarshguptamaurya@gmail.com>
2020-10-31 00:02:08 +0530

    Update index_task.py

--- index_task.py
+++ index_task.py
@@ -9,7 +9,7 @@ import os
 
 global_init()
 bc = BertClient(output_fmt='list')
-client = Elasticsearch('13.68.241.106:9200')
+client = Elasticsearch(globals.ELASTIC_SEARCH_HOST)
 
 def getVal(db_obj, key: str, error_res=""):
     try:
